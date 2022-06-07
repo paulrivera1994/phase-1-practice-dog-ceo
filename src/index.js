a4b8cb863c557c07325c2e3c725292e37b8faef4
Paul Rivera <juniorrivera81@yahoo.com>
2022-06-07 13:10:33 -0600

    commit tree 6137d078965a4e7986ad54df9c9c7f5912c1cd8c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+let breeds = [];
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages()
+    loadBreedOptions()
+});
+
+function loadImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(results => {
+            results.message.forEach(image => addImage(image))
+        });
+}
+function addImage(dogPicUrl) {
+    let container = document.querySelector("#dog-image-container")
+    let newImageEle = document.createElement('img');
+    newImageEle.src = dogPicUrl;
+    container.appendChild(newImageEle)
+}   
